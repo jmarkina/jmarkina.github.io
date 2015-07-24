ca46756426741f1b8e3c56a0512dfc89aa5fe8e9
Yulia Markina <jmarkina@gmail.com>
2015-07-24 12:30:59 -0700

    basic error handling

--- TodoList/store.js
+++ TodoList/store.js
@@ -1,52 +1,70 @@
 var items = []
+var errors = []
 
 var notifyComponents = function() {
   $(ListStore).trigger('storeHasChanged')
+}
+
+var showErrors = function() {
+  $(ListStore).trigger('hasErrors');
 }
 
 var findItemById = function(id) {
   return items.filter(function(item) {
     return item.id === id
   })[0]
-},
+}
 
 ListStore = {
+  listName: 'NickHongList',
+
+  urlRoot: function() {
+    return "http://listalous.herokuapp.com/lists/" + this.listName;
+  },
 
   getItems: function() {
     return items
   },
 
-  loadItems: function(listName) {
-  var loadRequest = $.ajax({
-    type: 'GET',
-    url: "https://listalous.herokuapp.com/lists/" + listName + "/"
-  })
+  loadItems: function() {
+    var loadRequest = $.ajax({
+      type: 'GET',
+      url: this.urlRoot()
+    })
+
     loadRequest.done(function(dataFromServer) {
-    items = dataFromServer.items
-    notifyComponents()
-  })
-},
+      items = dataFromServer.items
+      notifyComponents()
+    })
+
+    loadRequest.error(function(err) {
+      errors = err.responseText;
+      $(ListStore).trigger('hasErrors')
+    })
+  },
   addItem: function(itemDescription) {
     var creationRequest = $.ajax({
       type: 'POST',
-      url: "http://listalous.herokuapp.com/lists/MyList/items",
+      url: this.urlRoot() + "/items",
       data: { description: itemDescription, completed: false }
     })
-      creationRequest.done(function(itemDataFromServer) {
-      items.push(itemDataFromServer)
-      notifyComponents()
+
+    creationRequest.done(function(itemDataFromServer) {
+      items.push(itemDataFromServer);
+      notifyComponents();
     })
   },
   toggleCompleteness: function(itemId) {
-    var item = findItemById(itemId)
+    var item = findItemById(itemId);
     var currentCompletedValue = item.completed
 
     var updateRequest = $.ajax({
       type: 'PUT',
-      url: "https://listalous.herokuapp.com/lists/MyList/items/" + itemId,
+      url: this.urlRoot() + "/items/" + itemId,
       data: { completed: !currentCompletedValue }
     })
-      updateRequest.done(function(itemData) {
+
+    updateRequest.done(function(itemData) {
       item.completed = itemData.completed
       notifyComponents()
     })
@@ -55,14 +73,22 @@ ListStore = {
     var item = findItemById(itemId);
 
     var deleteRequest = $.ajax({
-      type: "DELETE",
-      url: "https://listalous.herokuapp.com/lists/MyList/items/" + itemId,
-
+      type: 'DELETE',
+      url: this.urlRoot() + "/items/" + itemId,
     })
-      deleteRequest.done(function(deletedItemData) {
-        var indexOfitem = items.indexOf(item);
-        items.splice(indexOfitem,1);
-        notifyComponents();
-      })
-    }
+
+    deleteRequest.done(function(deletedItemData) {
+      var indexOfItem = items.indexOf(item)
+      items.splice(indexOfItem,1);
+      notifyComponents();
+    })
+  },
+
+  setErrors: function(newErrors) {
+    errors = newErrors;
+  },
+
+  getErrors: function() {
+    return errors;
+  }
 }
